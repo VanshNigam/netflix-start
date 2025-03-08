23b1fbd4c270d170d466436a18bca46eefe2056a
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-08 16:26:34 +0530

    Add files via upload

---
+++ src/Components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div className="">
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/42a0bce6-fc59-4c1c-b335-7196a59ae9ab/web/IN-en-20250303-TRIFECTA-perspective_d5f81427-d6cf-412d-8e86-2315671b9be1_large.jpg"
+          alt="back"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
