a662a7574282fe73caf35aa9a1269fb2df49ec23
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-11 03:02:27 +0530

    Add files via upload
    sign in

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSign, setisSign] = useState(true);
+
+  const toggleSignInForm = () => {
+    console.log("click");
+    setisSign(!isSign);
+  };
   return (
     <div className="">
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/42a0bce6-fc59-4c1c-b335-7196a59ae9ab/web/IN-en-20250303-TRIFECTA-perspective_d5f81427-d6cf-412d-8e86-2315671b9be1_large.jpg"
           alt="back"
         />
       </div>
+      <form className="absolute p-12 text-white bg-black  bg-opacity-80 w-3/12 my-36 mx-auto right-0 left-0 py-6">
+        <h1 className="font-bold text-3xl">
+          {" "}
+          {isSign ? "Sign in" : "Sign up"}
+        </h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        {!isSign && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button
+          type="button"
+          className="p-4 my-6 w-full bg-red-700 "
+          onClick={toggleSignInForm}
+        >
+          {isSign ? "Sign in" : "Sign up"}
+        </button>
+        <p className="text-sm text-center">
+          {isSign
+            ? "New to NetFlix ? Sign Up Now"
+            : "Already registered ? Sign in"}
+        </p>
+      </form>
     </div>
   );
 };
